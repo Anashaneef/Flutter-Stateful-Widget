3ded12fd49f9e42c676183931664e22eb69178e7
Anashaneef <anas.f.hanif@students.amikom.ac.id>
2022-01-21 11:40:06 +0700

    First Commit

---
+++ android/app/src/main/kotlin/com/example/p4_stateful_widget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.p4_stateful_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int number = 0;
+  void tekantombol() {
+    setState(() {
+      number++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Stateful Widget Demo"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                number.toString(),
+                style: TextStyle(fontSize: 10 + number.toDouble()),
+              ),
+              RaisedButton(
+                child: Text("Tambah Bilangan"),
+                onPressed: tekantombol,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
